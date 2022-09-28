8baa8ba902d51980ad2b5e617feb1d53a1eb66fc
joshplantin <joshplantin@gmail.com>
2022-09-27 19:07:15 -0500

    Second Commit

--- src/gitclass03b14/Class03.java
+++ src/gitclass03b14/Class03.java
@@ -1,5 +1,10 @@
 package gitclass03b14;
 
 public class Class03 {
-
+public static void main(String[] args) {
+	System.out.println("This is git class");
+	System.out.println("Hey this is a change in my project");
+	System.out.println("This is the second change");
+	
+}
 }
